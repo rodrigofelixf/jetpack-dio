f0965f97be135e1f38efaddb14fb7395f5fb5e21
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-02-05 22:05:05 -0300

    Update Material Disgner

--- app/src/main/java/br/com/messoretech/MainActivity.kt
+++ app/src/main/java/br/com/messoretech/MainActivity.kt
@@ -13,5 +13,15 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        insertListeners()
+
     }
+
+    private fun insertListeners() {
+        binding.cvButtons.setOnClickListener {
+            startActivity(ButtonsActivity.createIntent(this))
+        }
+    }
+
 }
