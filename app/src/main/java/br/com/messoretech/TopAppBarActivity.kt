2c14bbcaa198d2ace8b78afdfc0d3c16dc8e6ced
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-02-14 12:49:10 -0300

    update final top app bar

--- app/src/main/java/br/com/messoretech/TopAppBarActivity.kt
+++ app/src/main/java/br/com/messoretech/TopAppBarActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.messoretech.databinding.ActivityTopAppBarBinding
+import br.com.messoretech.extenstions.toast
 
 class TopAppBarActivity : AppCompatActivity() {
     private val binding by lazy { ActivityTopAppBarBinding.inflate(layoutInflater) }
@@ -12,6 +13,28 @@ class TopAppBarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.topAppBar.setNavigationOnClickListener {
+            toast("Clicou no menu de navegacao")
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.favorite -> {
+                    toast("Click Favorite")
+                    true
+                }
+                R.id.search -> {
+                    toast("Click Search")
+                    true
+                }
+                R.id.more -> {
+                    toast("Click more")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     companion object {
